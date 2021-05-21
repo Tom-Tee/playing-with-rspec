454d7510001f18410053971a1f6346a24e793f14
Thomas Temple <tjt_84@hotmail.com>
2021-05-21 17:48:10 +1000

    added a sample test

---
+++ spec/udemy_test.rb
@@ -0,0 +1,10 @@
+
+
+describe "math calculations" do
+  it "does basic math" do
+    expect(2+2).to eq(4)
+    expect(4*4).to eq(16)
+    expect(10-5).to eq(5)
+    expect(10x10).to eq(100)
+  end
+end
